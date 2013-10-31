b0bf6f345109512221d4eb91db093ad73ffa4159
lachauj <lachaud.jeremy@gmail.com>
2013-10-31 22:24:41 +0000

    add feh browsing

--- picloc.bash
+++ picloc.bash
@@ -1,24 +1,73 @@
 #!/usr/bin/env bash
 
-test $# -lt 2 && echo "usage: $0 <path> <match>" && exit 1
+DIR="./"
+MATCH="*.jpg"
+FEH=0
 
-export PATH="$PATH:/usr/bin/vendor_perl/"
+declare -f show_metadata
 
-DIR="$1"
-MATCH="$2"
-
-find "$DIR" -iname "$MATCH" | {
-    while read str;
-    do
-	GPS=$( exiftool -n -c "%.6f degrees" "$str" | grep -i "gps position") 
-	if [ "$GPS" != "" ];
-	then
-	    str=$(echo $str | sed -e 's/\ /%20/g')
-	    echo "File: file://$str" 
-	    ll=$(echo "$GPS" | cut -d ':' -f2 | sed -e 's/\ //' -e 's/\ /%2C/')
-	    address=$(curl "http://maps.googleapis.com/maps/api/geocode/json?latlng=$ll&sensor=false" 2>/dev/null | grep -i "formatted_address" | head -n 1 | cut -d ':' -f2 | sed -e 's/\ "//' -e 's/",//')
-	    echo "Address: $address"
-	    echo -e "Maps url: https://www.google.fr/maps/preview#!q=$ll\n"
-	fi
-    done
+function usage() {
+    echo "usage: $0 [options [arg] ]"
+    echo -e "\t-h display this help"
+    echo -e "\t-x browsing with feh"
+    echo -e "\t-p change default path (./)"
+    echo -e "\t-x change default match (\"*.jpg\")"
 }
+
+function show_metadata() {
+    echo "salut"
+}
+
+function find_pics() {
+
+    find "$DIR" -iname "$MATCH" | {
+	while read str;
+	do
+	        GPS=$( exiftool -n -c "%.6f degrees" "$str" | grep -i "gps position") 
+		    if [ "$GPS" != "" ]; then
+			str_encode=$(echo $str | sed -e 's/\ /%20/g')
+			echo "File: file://$str_encode" 
+			ll=$(echo "$GPS" | cut -d ':' -f2 | sed -e 's/\ //' -e 's/\ /%2C/')
+			address=$(curl "http://maps.googleapis.com/maps/api/geocode/json?latlng=${ll}&sensor=false" 2>/dev/null | grep -i "formatted_address" | head -n 1 | cut -d ':' -f2 | sed -e 's/\ "//' -e 's/",//')
+			echo "Address: $address"
+			echo -e "Maps url: https://www.google.fr/maps/preview#!q=$ll\n"
+
+			if [ $FEH ]; then
+			        feh --info "curl \"http://maps.googleapis.com/maps/api/geocode/json?latlng=${ll}&sensor=false\" 2>/dev/null | grep -i \"formatted_address\" | head -n 1 | cut -d ':' -f2 | sed -e 's/\ \"//' -e 's/\",//' -e 's/,\ /,/g' | tr ',' '\n'" "${str}"
+				fi
+			    fi
+		    done
+    }
+}
+
+function main() {
+
+    while getopts .xhp:m:. OPTION
+    do
+        case $OPTION in
+                h)
+		usage
+		exit 0
+		;;
+                x)
+                FEH=1
+                ;;
+                p)
+		DIR=$OPTARG
+                ;;
+                m)
+                MATCH=$OPTARG
+                ;;
+                ?)
+                usage
+                exit 1
+                ;;
+            esac
+    done
+
+    export PATH="$PATH:/usr/bin/vendor_perl/"
+
+    find_pics
+}
+
+main "${@}"
