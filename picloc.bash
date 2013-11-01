e8057a1e408a808311b77656e28b125f57733c79
lachauj <lachaud.jeremy@gmail.com>
2013-11-01 01:02:09 +0000

    add convert mode

--- picloc.bash
+++ picloc.bash
@@ -3,11 +3,13 @@
 DIR="./"
 MATCH="*.jpg"
 FEH=0
+CONVERT=0
 
 function usage() {
     echo "usage: $0 [options [arg] ]"
     echo -e "\t-h display this help"
     echo -e "\t-x viewing with feh"
+    echo -e "\t-c convert the image with location annotation"
     echo -e "\t-p change default path (./)"
     echo -e "\t-x change default match (\"*.jpg\")"
 }
@@ -30,8 +32,12 @@ function find_pics() {
 		if [ $FEH == 1 ]; then
 		    feh --info "curl \"http://maps.googleapis.com/maps/api/geocode/json?latlng=${ll}&sensor=false\" 2>/dev/null | grep -i \"formatted_address\" | head -n 1 | cut -d ':' -f2 | sed -e 's/\ \"//' -e 's/\",//' -e 's/,\ /,/g' | tr ',' '\n'" "${str}" 2>/dev/null
 		fi
-	    fi
 
+		if [ $CONVERT == 1 ]; then
+		    convert "${str}" -gravity NorthWest -annotate 0 "$(echo $address | sed -e 's/,\ /,/g' | tr ',' '\n')"  $(basename "${str}" | sed -e 's/\(\.[A-Za-z]\{0,8\}$\)/-gps\1/')
+		fi
+	    fi
+	    
 	    sleep 1
 	done
     }
@@ -39,23 +45,26 @@ function find_pics() {
 
 function main() {
 
-    while getopts .xhp:m:. OPTION
+    while getopts .xchp:m:. OPTION
     do
         case $OPTION in
-                h)
+            h)
 		usage
 		exit 0
 		;;
-                x)
+            x)
                 FEH=1
                 ;;
-                p)
+            c)
+                CONVERT=1
+                ;;
+            p)
 		DIR=$OPTARG
                 ;;
-                m)
+            m)
                 MATCH=$OPTARG
                 ;;
-                ?)
+            ?)
                 usage
                 exit 1
                 ;;
